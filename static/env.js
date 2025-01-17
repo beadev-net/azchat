65951b874e8a2a7ca77274bafd00a3b30134f4f9
Vinicius Morais Dutra <viniciusmd@ciandt.com>
2025-01-16 22:25:12 -0300

    wip: env.js

---
+++ static/env.js
@@ -0,0 +1,11 @@
+// index.html
+const CLIENT_ID = "d3dd6d62-db7f-4bcf-9e82-f6d1fe33484e";
+const REDIRECT_URI =
+  "https://black-rock-08ad79f10.4.azurestaticapps.net/chat.html";
+const AUTH_ENDPOINT =
+  "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
+
+// chat.html
+const BACKEND_HOST =
+  "wss://azchat-fpecdbdadcd7frhc.eastus-01.azurewebsites.net";
+const GRAPH_API_URL = "https://graph.microsoft.com/v1.0/me";
